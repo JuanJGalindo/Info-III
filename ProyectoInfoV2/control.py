25f7a8b1e6f22b36a0410ad23a7aeb078041e759
JuanJGalindo <105535721+JuanJGalindo@users.noreply.github.com>
2022-12-02 13:15:13 -0500

    Add files via upload

---
+++ ProyectoInfoV2/control.py
@@ -0,0 +1,39 @@
+"""
+El módulo Control realiza la integración del módulo de Lógica y de Interfaz. De forma adicional, implementa
+ una herramienta de corrección de ingreso de datos para el usuario y evalúa la posibilidad de la irresolubilidad de un sistema de ecuaciones.
+"""
+import numpy as np
+import pandas as pd
+import logica as log
+import interfaz as interf
+
+interf.explicarLogica(), print()
+
+
+while True: 
+    n=int(input('Ingrese el número de ecuaciones del sistema a resolver: '))
+    datos=log.ingresoDatos(n), print()
+
+    a=datos[0][0]
+    b=datos[0][1]
+
+    print('La matriz de coeficientes es: ')
+    interf.imprimirMatriz(a, n)
+    print('El vector de términos independientes es: ')
+    interf.imprimirVector(b, n), print()
+
+    validez=''
+    validez=input('Si la matriz y el vector anterior son correctos, oprima el caracter "w". Si no, digite el caracter "l" y reescriba todo nuevamente: ')
+    if validez[0]=='l': continue
+    elif validez[0]=='w': break
+
+
+resultado=log.resolverSistema(a, b, n)
+
+if np.any(resultado)==False:
+    mensajeError='El sistema de ecuaciones es irresoluble.'
+    print(), print(mensajeError)
+else:
+    print(),print('Los valores resultantes para las variables involucradas en el sistema de ecuaciones lineales son:')
+    interf.imprimirResultado(resultado, n)
+
---
+++ ProyectoInfoV2/logica.py
@@ -0,0 +1,47 @@
+"""
+El módulo Lógica contiene las siguientes dos funciones:
+  - ingresoDatos: Recibe el parámetro 'n', como el número de ecuaciones que posee el sistema.
+                  Retorna una tupla con la matriz numpy 'a' de coeficientes y el vector numpy 'b' con los términos independientes.
+
+  - resolverSistema: Recibe tres parámetros: El parámetro 'a' como la matriz de coeficientes del sistema, a través de una matriz numpy.
+                                            El parámetro 'b' como el vector de términos independientes del sistema, a través de un vector numpy.
+                                            El parámetro 'n' como el número de ecuaciones del sistema de ecuaciones.
+                     Retorna un vector columna numpy con los valores de las variables solución del sistema.
+"""
+import numpy as np
+import pandas as pd
+
+def ingresoDatos(n):
+    data1=[]
+    data2=[]
+    a=[]
+    b=[]
+    for i in range(n**2):
+        temp=float(input('Ingrese el coeficiente de la posición determinada para la matriz de coeficientes (%s): '%(i+1)))
+        data1.append(temp)
+    a=np.array(data1).reshape(n,n)
+
+    for i in range(n):
+        temp=float(input('Ingrese el coeficiente de la posición determinada para el vector de términos independientes:'))
+        data2.append(temp)
+    b=np.array(data2).reshape(n,1)
+
+    return a, b
+
+def resolverSistema(a, b, n):
+
+#Fase de Eliminación
+    for k in range(0,n-1):
+        for i in range(k+1,n):
+            if a[i,k] != 0.0:
+                lam = a[i,k]/a[k,k]
+                a[i,k+1:n] = a[i,k+1:n] - lam*a[k,k+1:n]
+                b[i] = b[i] - lam*b[k]
+
+#Fase de Sustitución
+    for k in range(n-1,-1,-1):
+        if a[k,k]!=0.0:
+            b[k] = (b[k] - np.dot(a[k,k+1:n],b[k+1:n]))/a[k,k]
+        else:
+            return False
+    return b
